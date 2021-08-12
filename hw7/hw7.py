e53c87a7c5cec94dcb2beb4b9cd2d211dd2b0881
Mark Okulov <mvokulov@gmail.com>
2021-08-12 12:35:43 +0000

    add hw7 rename hw8

--- hw7/hw7.py
+++ hw7/hw7.py
@@ -1,27 +1,60 @@
-# 1. После запуска предлагает пользователю ввести текст.
-# 2. Проверяет и, если возможно, преобразовывает полученный текст в число,
-# используя рекурсивную функцию.
-# Если число четное - делит его на 2 и выводит результат.
-# Если число нечетное - умножает на 3 и прибавляет 1.
-# После чего ждет следующего ввода.
-# 3.При получении в качестве вводных данных 'cancel' завершает свою работу.
-import re
-def loop():
-    userinput = input()
-    if userinput == 'cancel':
-        print("Bye!")
-        return
-    else:
-        # Можно сделать через try-except, но лучше не обрабатывать исключения, а избегать их
-        if re.match(r'^\d+$', userinput):
-            num = int(userinput)
-            if num % 2 == 0:
-                print(num // 2)
-            else:
-                print(num * 3 + 1)
-        else:
-            print("Не удалось преобразовать введенный текст в число.")
-    return loop()
+# Напишите программу, которая читает данные из файлов
+# /etc/passwd и /etc/group на вашей системе и выводит
+# следующую информацию в файл output.txt:
+# 1. Количество пользователей, использующих все имеющиеся
+# интерпретаторы-оболочки.
+# ( /bin/bash - 8 ; /bin/false - 11 ; ... )
+# 2. Для всех групп в системе - UIDы пользователей
+# состоящих в этих группах.
+# ( root:1, sudo:1001,1002,1003, ...)
 
-loop()
-        
+passwd = open('passwd', 'r')
+groups = open('group', 'r')
+output = open('output.txt', 'w')
+
+class User:
+    def __init__(self, username, uid, pgid):
+        self.username = username
+        self.uid = uid
+        self.pgid = pgid
+
+shellsdict = {}
+groupsdict = {}
+uidict = {}
+userspgids = []
+
+for line in passwd:
+    line = line.split(':')
+    shell = line[-1].replace('\n','')
+    if shell in shellsdict:
+        shellsdict[shell] += 1
+    else:
+        shellsdict[shell] = 1
+    user = line[0]
+    uid = line[2]
+    pgid = line[3]
+    userspgids.append(User(user, uid, pgid))
+    uidict[user] = uid
+
+for line in groups:
+    line = line.split(':')
+    group = line[0]
+    gid = line[2]
+    users = line[-1][:-1].split(',')
+    groupsdict[group] = []
+    if users != ['']:
+        groupsdict[group] = [uidict[user] for user in users]
+    # Если группа - primary для пользователя, то его username в /etc/groups не указыватся
+    for user in userspgids:
+        if gid == user.pgid:
+            groupsdict[group].append(user.uid)
+
+for shell in shellsdict:
+    output.write(f'{shell} - {shellsdict[shell]}\n')
+output.write('\n')
+for group in groupsdict:
+    output.write(f'{group}: {" ".join(groupsdict[group])}\n')
+
+passwd.close()
+groups.close()
+output.close()
