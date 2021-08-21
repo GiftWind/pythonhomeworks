39d8f8673bdccf9c559d5be14b2677f1e3807502
Mark Okulov <mvokulov@gmail.com>
2021-08-21 09:24:55 +0000

    add pure one-liner for hw9 problem40

--- hw09/hw9-pe40.py
+++ hw09/hw9-pe40.py
@@ -4,5 +4,9 @@
 # If dn represents the nth digit of the fractional part, find the value of the following expression.
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 
+# Решение с помощью left fold (однострочник если import не считается):
 import functools
 print(functools.reduce(lambda x,y: int(x) * int(y), (''.join(str(i) for i in range(1, 200000))[i-1] for i in [10 ** i for i in range(7)])))
+
+# И чистый однострочник:
+print(eval('*'.join([''.join(str(i) for i in range(1, 200000))[i-1] for i in [10 ** i for i in range(7)]])))
