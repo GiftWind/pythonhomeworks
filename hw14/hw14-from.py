92afab7c44f034d46e4da36b79bbd7066e0b2eaf
Mark Okulov <mvokulov@gmail.com>
2021-08-21 13:44:08 +0000

    add hw14

--- hw14/hw14-from.py
+++ hw14/hw14-from.py
@@ -2,3 +2,12 @@
 # Employee и перенести его в другую программу,
 # используя модуль Pickle и файловую систему.
 # Узнать про + и - модуля Pickle.
+
+import pickle
+from Employee import Employee
+
+alice = Employee('Alice Wonderland', '+7(999)999-9999', 19999)
+f = open('alice', 'wb')
+pickle.dump(alice, f)
+f.close()
+alice.print_salary_info()
